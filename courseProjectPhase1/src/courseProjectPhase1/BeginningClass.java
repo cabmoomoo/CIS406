4d7550b7d8d83fefeadb02c471973affc320cb9a
cabmoomoo <68883100+cabmoomoo@users.noreply.github.com>
2023-10-08 14:17:38 -0400

    Initial Commit
    Week 1 work
    Git setup

---
+++ courseProjectPhase1/src/courseProjectPhase1/BeginningClass.java
@@ -0,0 +1,12 @@
+package courseProjectPhase1;
+
+public class BeginningClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		System.out.println();
+		System.out.println("congratulations on your first Java program");
+	}
+
+}
