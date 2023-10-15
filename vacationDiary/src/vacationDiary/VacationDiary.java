25eb984d98218d78e97eda9c5522a9abc4637878
cabmoomoo <68883100+cabmoomoo@users.noreply.github.com>
2023-10-14 20:56:53 -0400

    Week 2 Lab 2: Vacation Diary

---
+++ vacationDiary/src/vacationDiary/VacationDiary.java
@@ -0,0 +1,24 @@
+package vacationDiary;
+
+public class VacationDiary {
+
+	public static void main(String[] args) {
+		System.out.println("Vacation Diary\n");
+		
+		System.out.println("Enter Date Vaction Started (mm/dd/yyyy): " + args[0]);
+		System.out.println("Enter City Visited: " + args[1]);
+		System.out.println("Enter Country Visited: " + args[2]);
+		System.out.println("Enter number of days: " + args[3]);
+		System.out.println("Enter Mode of Travel (car, airplane, ship, train, bus): " + args[4]);
+		System.out.println("");
+		
+		System.out.println("Details of your trip:");
+		System.out.println("Date: " + args[0]);
+		System.out.println("City: " + args[1]);
+		System.out.println("Country: " + args[2]);
+		System.out.println("Number of Days: " + args[3]);
+		System.out.println("Traveled By: " + args[4]);
+		System.out.println("Bye!!!");
+	}
+
+}
