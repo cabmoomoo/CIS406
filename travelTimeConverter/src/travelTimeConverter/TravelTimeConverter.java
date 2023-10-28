daccdedca5cd275639f472739d31782b0d1c8267
cabmoomoo <68883100+cabmoomoo@users.noreply.github.com>
2023-10-28 15:33:28 -0400

    Week 4 Lab 2: Travel Time Converter
    Also, the prior change to the week 1 project was due to me accidentally
    making a new project based in the high-level folder. Whoops.

---
+++ travelTimeConverter/src/travelTimeConverter/TravelTimeConverter.java
@@ -0,0 +1,31 @@
+//Caleb Barbee CIS406 Travel Time Converter Lab
+package travelTimeConverter;
+import java.util.Scanner;
+
+public class TravelTimeConverter {
+
+	public static void main(String[] args) {
+
+		System.out.println("Welcome to the Travel Time Calculator\n");
+		Scanner userInput = new Scanner(System.in);
+		
+		System.out.print("Enter miles: ");
+		float milesInput = userInput.nextFloat();
+		System.out.print("Enter miles per hour: ");
+		float mphInput = userInput.nextFloat();
+		
+		System.out.println("Estimated travel time");
+		System.out.println("---------------------");
+		
+		float estHours = milesInput / mphInput;
+//		int finHours = (int) estHours;
+		float estMinutes = (milesInput % mphInput) / mphInput * 60f;
+		
+		System.out.println("Hours: " + (int) estHours);
+		System.out.println("Minutes: " + (int) estMinutes);
+		
+		System.out.println("\nSEE YOU SPACE COWBOY...");
+
+	}
+
+}
